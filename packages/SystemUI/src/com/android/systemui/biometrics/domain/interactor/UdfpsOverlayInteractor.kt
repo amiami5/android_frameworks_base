07c04537ead76c9bb6b8f8bf2a7cea4eaac15eb5
Ido Ben-Hur <idoybh2@gmail.com>
2024-10-20 15:05:04 +0300

    SystemUI: Reduce padding on the UDFPS foreground icon
    should be bigger relative to sensor / background

--- packages/SystemUI/src/com/android/systemui/biometrics/domain/interactor/UdfpsOverlayInteractor.kt
+++ packages/SystemUI/src/com/android/systemui/biometrics/domain/interactor/UdfpsOverlayInteractor.kt
@@ -104,7 +104,7 @@ constructor(
     val iconPadding: Flow<Int> =
         udfpsOverlayParams.map { params ->
             val sensorWidth = params.nativeSensorBounds.right - params.nativeSensorBounds.left
-            val nativePadding = (sensorWidth - iconSize) / 2
+            val nativePadding = (sensorWidth - iconSize) / 5
             (nativePadding * params.scaleFactor).toInt()
         }
 
